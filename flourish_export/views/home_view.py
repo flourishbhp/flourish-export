e9ea01b4fdbf9963cac6837dc51d94a1a9fbc50c
Coulson Kgathi <ckgathi@gmail.com>
2021-08-17 06:42:34 +0200

    export data order exports

--- flourish_export/views/home_view.py
+++ flourish_export/views/home_view.py
@@ -55,11 +55,11 @@ class HomeView(ListBoardViewMixin, EdcBaseViewMixin,
                                  thread_target=self.download_child_data,
                                  description='Flourish Child CRF Export')
         non_crf_exports = ExportFile.objects.filter(
-            description='Flourish Non CRF Export').order_by('uploaded_at')
+            description='Flourish Non CRF Export').order_by('-uploaded_at')
         caregiver_crf_exports = ExportFile.objects.filter(
-            description='Flourish Caregiver CRF Export').order_by('uploaded_at')
+            description='Flourish Caregiver CRF Export').order_by('-uploaded_at')
         child_crf_exports = ExportFile.objects.filter(
-            description='Flourish Child CRF Export').order_by('uploaded_at')
+            description='Flourish Child CRF Export').order_by('-uploaded_at')
         context.update(
             non_crf_exports=non_crf_exports,
             caregiver_crf_exports=caregiver_crf_exports,
