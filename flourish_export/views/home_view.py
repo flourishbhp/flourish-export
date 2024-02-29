9bc5c87d291ce373e482902b0862ac023235180e
Ame Diphoko <diphokoame@gmail.com>
2024-02-29 16:35:24 +0200

    add: move export methods to mixin for reusability

--- flourish_export/views/home_view.py
+++ flourish_export/views/home_view.py
@@ -1,16 +1,16 @@
-from django.contrib import messages
 from django.views.generic import TemplateView
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_navbar import NavbarViewMixin
 
 from ..admin_export_helper import AdminExportHelper
-from ..tasks import generate_exports
 from ..identifiers import ExportIdentifier
 from ..models import ExportFile
 from .listboard_view_mixin import ListBoardViewMixin
+from .export_methods_view_mixin import ExportMethodsViewMixin
 
 
-class HomeView(ListBoardViewMixin, EdcBaseViewMixin,
+class HomeView(ExportMethodsViewMixin,
+               ListBoardViewMixin, EdcBaseViewMixin,
                NavbarViewMixin, TemplateView):
 
     template_name = 'flourish_export/home.html'
@@ -38,34 +38,3 @@ class HomeView(ListBoardViewMixin, EdcBaseViewMixin,
             caregiver_crf_exports=caregiver_crf_exports,
             child_crf_exports=child_crf_exports)
         return context
-        
-    def create_export_obj(self, app_label, ):
-        export_identifier = self.export_identifier_cls().identifier
-
-        description = f'{app_label.replace("_", " ").title()} Export(s)'
-        model_options = {
-            'description': description,
-            'study': app_label,
-            'export_identifier': export_identifier, }
-
-        ExportFile.objects.create(**model_options)
-        return export_identifier
-        
-    def generate_export(self, app_label, ):
-        app_list = self.admin_helper_cls().get_app_list(app_label)
-        app_list = self.admin_helper_cls().remove_exclude_models(app_list)
-        user_emails = [self.request.user.email]
-
-        try:
-            ExportFile.objects.get(study=app_label,
-                                   download_complete=False)
-        except ExportFile.DoesNotExist:
-            export_identifier = self.create_export_obj(app_label)
-            generate_exports.delay(app_list, True, user_emails, export_identifier)
-            message = (f'{app_label.replace("_", " ").capitalize()} export has been initiated, '
-                       'an email will be sent once download completes.')
-        else:
-            message = (f'Download for {app_label.replace("_", " ").capitalize()} that was '
-                        'initiated is still running. Please wait until an export is fully prepared.')
-                
-        messages.add_message(self.request, messages.INFO, message)
